2a013a8a62b12501a083ae4645ded42d28a666d8
Severin Langberg <langberg91@gmail.com>
2023-11-16 13:25:52 +0100

    fig formatting

--- evaluation.py
+++ evaluation.py
@@ -4,16 +4,17 @@ import matplotlib.pyplot as plt
 
 def plot_loss(loss, path_to_fig, ref_loss=None, title=None):
 
-	plt.figure(figsize=(8, 6))
+	plt.figure(figsize=(5, 4))
 
-	plt.plot(loss, label="loss")
+	plt.plot(loss, label="Federated")
 	if ref_loss is not None:
-		plt.plot(ref_loss, label="reference loss")
+		plt.plot(ref_loss, label="Centralized")
 
 	if title is not None:
 		plt.title(title)
 
 	plt.xlabel("Sever epoch")
+	plt.ylabel("Aggregated loss value")
 
 	plt.legend()
 	plt.tight_layout()
@@ -22,16 +23,17 @@ def plot_loss(loss, path_to_fig, ref_loss=None, title=None):
 
 def plot_coefficients(coefs, path_to_fig, ref_coefs=None, title=None):
 
-	plt.figure(figsize=(8, 6))
+	plt.figure(figsize=(5, 4))
 
-	plt.plot(coefs, label="coefs", marker="o", linestyle="", markersize=8)
+	plt.plot(coefs, label="Federated", marker="o", linestyle="", markersize=8)
 	if ref_coefs is not None:
-		plt.plot(ref_coefs, label="reference coefs", marker="o", linestyle="", markersize=6)
+		plt.plot(ref_coefs, label="Centralized", marker="o", linestyle="", markersize=6)
 
 	if title is not None:
 		plt.title(title)
 
 	plt.xlabel("Coefficient number")
+	plt.xlabel("Coefficient value")
 
 	plt.legend()
 	plt.tight_layout()
